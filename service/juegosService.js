5cbe1a9f080f034a19ced8265ccf5f96f126cd5c
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-03-17 21:05:43 -0600

    APIBUSCAR

--- service/juegosService.js
+++ service/juegosService.js
@@ -16,17 +16,32 @@ const crearJuegos = async (name, year, company, score)=>{
 //Editar
 const editarJuegos = async (id, data)=>{
     try{
-        const juegoeditar = await editJuegos.findByIdAndUpdate(id, data);
+        const juegoeditar = await juegosModel.findByIdAndUpdate(id, data, {new:true});
         if(juegoeditar){
             return juegoeditar;
         }else{
             return ({error:'No es posible encontrar el juego'});
         }
     }catch(error){
-       return ({error:'No es posible editar el juego'});
+        console.error(error);
+       return ({error:error});
     }
 }
 //Buscar por Score 
+const buscarScore = async (score)=>{
+    try{
+        const juegoscore = await juegosModel.find({score})
+        if(juegoscore){
+            return juegoscore;
+        }else{
+            return({error:'No es posible encontrar el Score'});
+        }
+    }catch(error){
+        console.error(error);
+        return({error:error});
+    }
+}
+
 //Buscar por Nombre 
 //
-module.exports = {crearJuegos, editarJuegos};
+module.exports = {crearJuegos, editarJuegos, buscarScore};
