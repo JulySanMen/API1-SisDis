5887afa845006936188e6e748d391515f5def300
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-03-17 21:05:36 -0600

    APIBUSCAR

--- routes/juegosRoute.js
+++ routes/juegosRoute.js
@@ -1,8 +1,9 @@
-const {crearJuegosController, editarJuegosController} = require('../Controller/juegosController');
+const {crearJuegosController, editarJuegosController, buscarScoreController} = require('../Controller/juegosController');
 
 const router = require('express').Router();
 
 router.post('/crearGame', crearJuegosController);
 router.put('/editarGame/:id', editarJuegosController);
+router.get('/buscarScore/:score', buscarScoreController);
 
 module.exports = router;
