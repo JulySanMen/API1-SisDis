4adf38fe21e3a240bfed19234c53d136399d5fb2
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-03-19 19:08:45 -0600

    API1-USERS

--- model/juegosModel.js
+++ model/juegosModel.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema({
     name:{type:String, requiered:true},
     year:{type:Number,requiered:true},
     company:{type:String,requiered:true},
-    score:{type:Number,requiered:true}
 })
 
 const juegosModel = mongoose.model('juegos', Schema);
--- model/scoresModel.js
+++ model/scoresModel.js
@@ -2,8 +2,8 @@ const mongoose = require ('mongoose');
 
 const scoreSchema = new mongoose. Schema({
     nikname:{type:String, required:true},
-    score:{type:Number, required:true},
-    videogame:{type:String, required:true}
+    score:{type:Number,requiered:true},
+    videogame:{type:String,requiered:true},
 })
 const scoreModel = mongoose.model('score', scoreSchema);
 module.exports = {scoreModel};
